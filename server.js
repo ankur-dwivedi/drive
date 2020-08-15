89204ba0582485990ffe13091d5c4a06803aa4fd
ankur dwivedi <ankur.dwivedi199714@gmail.com>
2020-08-15 06:24:18 +0530

    initial commit3

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 let dummy=
     {
         "ok": true,
